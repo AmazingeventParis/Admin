050aa350dc656c577bc6c89997bedd56344f967c
aschercohen-a11y <aschercohen@gmail.com>
2026-02-07 00:19:30 +0100

    Fix Android package name (restore underscore)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.blockpuzzle"
+    namespace = "com.example.block_puzzle"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.blockpuzzle"
+        applicationId = "com.example.block_puzzle"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
